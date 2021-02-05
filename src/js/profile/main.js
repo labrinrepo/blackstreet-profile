3c73002e98a2cd0b7c8b267227733decdd109102
Habil99 <abiyevhabil3@gmail.com>
2021-02-05 22:26:33 +0400

    profile updated

--- src/js/profile/main.js
+++ src/js/profile/main.js
@@ -14,6 +14,24 @@ $(function () {
   $(".btn-edit");
   $(".cancel-btn").on("click", () => {
     $(".profile-info").removeClass("hide");
-    $(".change-pass, .edit-profile,.delete-profile").addClass("hide");
+    $(".change-pass, .edit-profile,.delete-profile,.delete-address").addClass(
+      "hide"
+    );
+  });
+  $(".btn-add-address").on("click", () => {
+    $(".address").addClass("hide");
+    $(".new-address").removeClass("hide");
+  });
+  $(".cancel-btn").on("click", () => {
+    $(".new-address").addClass("hide");
+    $(".address").removeClass("hide");
+  });
+  $("#saveNewAddress").on("click", () => {
+    $(".new-address").addClass("hide");
+    $(".my-addresses").removeClass("hide");
+  });
+  $(".close-table").on("click", () => {
+    $(".my-addresses").addClass("hide");
+    $(".delete-address").removeClass("hide");
   });
 });
